29711f1086caccef7dda97d0c706a0d9612574ed
Fahad Abdullah <134834352+FAbdullah17@users.noreply.github.com>
2025-02-07 11:31:38 +0500

    Final Commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="EasyML-Package",
+    name="EasyMLTool",
     version="0.1.0",
     author="Fahad Abdullah",
     author_email="fahadai.co@gmail.com",
